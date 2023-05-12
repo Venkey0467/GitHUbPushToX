5eb65ce6ed701b06e514062791fffd59ac80309b
Venkey0467 <s.venkataramana0353@gmail.com>
2023-05-12 00:19:46 -0700

    User_s_is_added

--- src/test/java/Git/AddUser.java
+++ src/test/java/Git/AddUser.java
@@ -6,14 +6,31 @@ import org.testng.annotations.Test;
 public class AddUser {
 
 	@Test
-	public void CreateUser()
+	public void CreateUser1()
 	{
 		given()
 			.contentType("application/json")
 			.body("{\r\n"
 					+ "    \"name\": \"boss\",\r\n"
-					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"role\": \"sm\",\r\n"
 					+ "    \"age\": 31,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+		.when()
+			.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+	
+	@Test
+	public void CreateUser2()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"venkey\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 28,\r\n"
 					+ "    \"city\": \"hyd\"\r\n"
 					+ "}")
 		.when()
